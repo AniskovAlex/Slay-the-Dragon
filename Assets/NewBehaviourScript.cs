6e189b9fc6be2760fb588a3494f081c10de76f96
GeorgeVorob <george77019721@gmail.com>
2021-07-06 21:49:43 +0700

    Update NewBehaviourScript.cs

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,7 +7,9 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        int x = 10;
+        int y = 10;
+        Debug.Log(x + y);
     }
 
     // Update is called once per frame
